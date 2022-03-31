60577707ffaa265257c54c42ef219b85692df62d
yrys <orunbaevusman@gmail.com>
2022-03-31 11:40:02 +0600

    myfirstcommit

---
+++ order/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Order
+class CreateOrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+    def clean_phone(self):
+        data = self.cleaned_data
+        phone = data.get('phone')
+        if not phone.startswith('+996'):
+            raise forms.ValidationError('Номер должен начинаться с +996')
+        if len(phone) != 13:
+            raise forms.ValidationError('Не правильный номер')
+
+
+        return phone
+
+
+
